122fcb7bcb7596aa83086c27db1d45e6fc49c63b
hasankhan.cse <hasankhan.cse@gmail.com>
2020-05-26 08:59:49 -0700

    Create basic stacture use components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import NavBar from './NavBar/NavBar';
+import ProductList from './ProductList/ProductList';
+import Cart from './Cart/Cart';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <div className="nav-bar">My Shop</div>
-      <div className="product-list">List</div>
-      <div className="cart">Cart</div>
+      <NavBar />
+      <ProductList />
+      <Cart />
     </div>
   );
 }
