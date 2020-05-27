22c1ace28d02b3013973ec445f967ccc8c7ce89e
hasankhan.cse <hasankhan.cse@gmail.com>
2020-05-27 04:33:24 -0700

    Drammy product add and apply style

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import NavBar from './NavBar/NavBar';
 import ProductList from './ProductList/ProductList';
 import Cart from './Cart/Cart';
+import data from './data';
 
 const App = () => {
   return (
     <div className="App">
       <NavBar />
-      <ProductList />
+      <ProductList products={data} />
       <Cart />
     </div>
   );
--- src/ProductList/ProductList.js
+++ src/ProductList/ProductList.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
-const ProductList = () => {
-  return <div className="product-list">List</div>;
+const Product = ({ id, title, brand, price, image_url }) => {
+	return (
+		<div className="product">
+  			<img src={image_url} alt={title} />
+  			<div className="title">
+  				<span>{title}</span>
+  				<span>{brand}</span>
+  			</div>
+  			<div className="actions">
+  				<span>$ {price}</span>
+  				<span>+Add to Cart</span>
+  			</div>
+  		</div>
+	);
+};
+
+const ProductList = ({ products }) => {
+  return <div className="product-list">
+  		{products.map(product => (
+  			<Product {...product} key={product.id} />
+  		))}
+  </div>;
 };
 
 export default ProductList;
